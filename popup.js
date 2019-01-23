6b2b72dfa7b69282da9f6f606842ec0d60f24bdb
Rodrigo Zanotta <rodrigozanottarsz@gmail.com>
2019-01-23 17:49:00 -0200

    added script to read the html from the active page (thanks stackoverflow)

--- popup.js
+++ popup.js
@@ -1,11 +1,22 @@
-'use strict';
-
-let changeColor = document.getElementById('changeColor');
-
-chrome.storage.sync.get('color', function(data) {
-
-    changeColor = document.getElementById('changeColor');
-
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+chrome.runtime.onMessage.addListener(function(request, sender) {
+    if (request.action == "getSource") {
+        message.innerText = request.source;
+    }
 });
+
+function onWindowLoad() {
+
+    var message = document.querySelector('#message');
+
+    chrome.tabs.executeScript(null, {
+        file: "getPagesSource.js"
+    }, function() {
+        // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+        if (chrome.runtime.lastError) {
+            message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+        }
+    });
+
+}
+
+window.onload = onWindowLoad;
